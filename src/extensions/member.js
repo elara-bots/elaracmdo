2fd65b23c589ea3e8d3d4e74a439436d1b856fda
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-05-08 00:26:49 -0700

    Update member.js

--- src/extensions/member.js
+++ src/extensions/member.js
@@ -4,7 +4,7 @@ const { Structures } = require("discord.js");
 module.exports = Structures.extend("GuildMember", GuildMember => {
     return class CommandoMember extends GuildMember {
         constructor(client, data, guild) {
-            super(client, data, guild);
+            super(client);
             this.pending = data.pending ?? false;
         };
     }
