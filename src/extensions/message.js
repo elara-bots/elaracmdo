2147c74f38821410a7ccf2cd76e804934161ca9c
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-05-28 22:03:15 -0700

    Update message.js

--- src/extensions/message.js
+++ src/extensions/message.js
@@ -21,7 +21,6 @@ module.exports = Structures.extend('Message', Message => {
 	class CommandoMessage extends Message {
 		constructor(client, data, channel) {
 			super(client, data, channel);
-			this.stickers = data.stickers ?? [];
 			/**
 			 * Whether the message contains a command (even an unknown one)
 			 * @type {boolean}
