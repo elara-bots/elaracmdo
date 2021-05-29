c71e20407d9f30267b5d08a838edf031da44b6da
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-05-28 22:03:45 -0700

    Delete member.js

--- src/extensions/member.js
+++
@@ -1,8 +0,0 @@
-module.exports = require("discord.js").Structures.extend("GuildMember", GM => {
-    return class CommandoMember extends GM {
-        constructor(client, data, guild) {
-            super(client, data, guild);
-            this.pending = data.pending ?? false;
-        };
-    };
-});
